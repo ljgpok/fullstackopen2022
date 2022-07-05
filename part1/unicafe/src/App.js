7fb9d2d1c24f782cd041001664ea4de3f9d9795d
Lal Gurung <ljgpok@yahoo.com>
2022-07-05 15:59:00 +0300

    part1 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
+
+const Statistics = (props) => {
+  let all = props.good + props.neutral + props.bad;
+  let average = (props.good - props.bad) / all;
+  let positive = (props.good / all) * 100 + " %";
+
+  if (all > 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.good} />
+            <StatisticLine text="neutral" value={props.neutral} />
+            <StatisticLine text="bad" value={props.bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={positive} />
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+};
+
+const Button = (props) => {
+  const increaseValue = () => props.setter(props.value + 1);
+  return <button onClick={increaseValue}>{props.text}</button>;
+};
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="feedback">
+        <h1>Give Feedback</h1>
+        <div className="buttons">
+          <Button text="good" value={good} setter={setGood} />{" "}
+          <Button text="neutral" value={neutral} setter={setNeutral} />{" "}
+          <Button text="bad" value={bad} setter={setBad} />{" "}
+        </div>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
