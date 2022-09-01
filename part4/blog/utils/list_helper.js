1d2fff669d404b7df7f114767759346d92399699
Lal Gurung <ljgpok@yahoo.com>
2022-09-01 23:02:24 +0300

    exercises 4.3-4.5

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,24 @@
+const dummy = (blogs) => {
+  return Number(blogs + 1);
+};
+
+const totalLikes = (blogs) => {
+  if (!Array.isArray(blogs) || !blogs.length) return 0;
+
+  if (blogs.length === 1) return blogs[0].likes;
+
+  return blogs.reduce((total, blog) => total + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+  return blogs.reduce((last_blog, current_blog) =>
+    current_blog.likes > last_blog.likes ? current_blog : last_blog
+  );
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
